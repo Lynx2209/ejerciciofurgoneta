412ed41e584da8227733932b6694451644a4970a
Lynx2209 <csi23-jconazo@colegioaltair.net>
2024-01-23 10:50:03 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ejerciciofurgoneta
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Cuanto pesa tu saco");
+            int pesodelSaco = Convert.ToInt32(Console.ReadLine());
+
+            int numerodelSacoMax = 4000 / pesodelSaco;
+            Console.WriteLine(numerodelSacoMax);
+        }
+    }
+}
